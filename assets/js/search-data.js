9986a7e4ccb8d5d806c6b7eead8e9061f46461df
AlirezaParchami <alirezaprm76@gmail.com>
2026-02-26 15:00:12 +0000

    Deploying to gh-pages from @ AlirezaParchami/AlirezaParchami.github.io@a7905e341fd03315c69e9ad2632087dff426f96e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of my R&amp;D and Fun Projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
