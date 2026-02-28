81a81299b8d4f94a05f32652422306361b611373
AlirezaParchami <alirezaprm76@gmail.com>
2026-02-28 14:39:21 +0000

    Deploying to gh-pages from @ AlirezaParchami/AlirezaParchami.github.io@814e39b2c21b998566b10d644da32eda5fe23bc0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -491,6 +491,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-research-amp-development-at-mercedes-benz-tech-innovation",
+          title: 'Research &amp;amp; Development at Mercedes-Benz Tech Innovation',
+          description: "ttt",
+          section: "Projects",handler: () => {
+              window.location.href = "/private/tech-details1/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
